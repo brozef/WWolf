6c054ddf3158775df708bef87465d2738a090885
Joe <10679867+brozef@users.noreply.github.com>
2020-10-29 10:53:45 +1000

    add initial category selection code

--- main.js
+++ main.js
@@ -21,8 +21,75 @@ function save_state() {
     window.sessionStorage.setItem('state', JSON.stringify(state));
 }
 
+load_state();
+
 // -----------------------------------------------------------
 // GAME
+
+//---- Topics
+const topics = {
+    space: {
+        icon: 'url("https://icons-for-free.com/iconfiles/png/512/astronomy+planet+science+space+icon-1320195089993574465.png")'
+    },
+    nature: {
+        icon: 'url("https://image.flaticon.com/icons/png/128/2990/2990966.png")'
+    }
+};
+
+let selectedTopics = [];
+
+function SelectTopic(topic) {
+
+}
+
+function SelectAllTopics() {
+    selectedTopics = [];
+    Object.keys(topics).forEach(key => {
+        selectedTopics.push(key);
+        let topicCheckbox = document.getElementById(key);
+        if (topicCheckbox) {
+            topicCheckbox.checked = true;
+        }
+    });
+}
+
+
+function DeselectAllTopics() {
+    selectedTopics = [];
+    Object.keys(topics).forEach(key => {
+        let topicCheckbox = document.getElementById(key);
+        if (topicCheckbox) {
+            topicCheckbox.checked = false;
+        }
+    });
+}
+
+function FillTopicList() {
+    let topicListElement = document.getElementById('topic-list');
+    if (topicListElement) {
+        topicListElement.innerHTML = '';
+
+        Object.keys(topics).forEach(key => {
+            let topicElement = document.createElement('li');
+            let topicCheckbox = document.createElement('input');
+            let topicLabel = document.createElement('label');
+
+            topicElement.style.backgroundImage = topics[key].icon;
+            topicCheckbox.id = key;
+            topicCheckbox.type = 'checkbox';
+            topicLabel.htmlFor = key;
+            topicLabel.innerText = key;
+
+            topicElement.appendChild(topicCheckbox);
+            topicElement.appendChild(topicLabel);
+            topicListElement.appendChild(topicElement);
+        });
+    }
+}
+
+//---- Options
+
+//---- Lobby
 
 
 // -----------------------------------------------------------
@@ -74,5 +141,3 @@ function Navigate(route) {
         console.error("attempted route does not exist", route);
     }
 }
-
-load_state();
